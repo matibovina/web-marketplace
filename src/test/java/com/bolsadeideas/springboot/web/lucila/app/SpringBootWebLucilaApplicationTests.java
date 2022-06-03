1d4009c068bf5c749d21e1d826776b2e8316ceef
Matias Bovina <matibovina@gmail.com>
2022-06-03 10:47:13 +0200

    modified spring test

--- src/test/java/com/bolsadeideas/springboot/web/lucila/app/SpringBootWebLucilaApplicationTests.java
+++ src/test/java/com/bolsadeideas/springboot/web/lucila/app/SpringBootWebLucilaApplicationTests.java
@@ -3,7 +3,7 @@ package com.bolsadeideas.springboot.web.lucila.app;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = {com.springboot.web.lucila.app.SpringBootWebLucilaApplication.class})
 class SpringBootWebLucilaApplicationTests {
 
 	@Test
